e352d0da97caf82dbfd9a699d0b8ebb2619ddad1
Tram Nguyen <tramvn1996@gmail.com>
2017-09-02 18:55:24 -0400

    Update increment-an-arbitrary-precision-integer.py

--- increment-an-arbitrary-precision-integer.py
+++ increment-an-arbitrary-precision-integer.py
@@ -1,3 +1,13 @@
 # Write a program that takes an array encoding a nonnegative decimal integer D, 
 # and update the array to represent the integer D+1
 def plus_one(A):
+    A[-1] += 1 
+    for i in reversed(range(1,len(A)):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1]  += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+    return A
