5a2bad167c1f032197f076a801a20eb965ca4a46
Tram Nguyen <tramvn1996@gmail.com>
2017-09-02 22:51:34 -0400

    change syntax

--- dutch_flag_partition.py
+++ dutch_flag_partition.py
@@ -1,18 +1,18 @@
 #the dutch flag problem
-#partition an array according to a pivot point 
+#partition an array according to a pivot point
 #(all elements before the pivot point have to be smaller than it, and the elements after have to be smaller
 
-def dutch_flag_parition(pivot_index, A)
+def dutch_flag_parition(pivot_index, A):
     pivot = A[pivot_index]
     #keep the following invariants during partitioning:
     # bottom group: A[:smaller]
     # middle group: A[smaller:equal]
     # unclassified group A[equal:larger]
     # top group: A[larger:]
-    
+
     smaller, equal, larger = 0, 0, len(A)
     #keep iterating as long as there is an unclassified element
-    
+
     while equal < larger:
       #A[equal] is the incoming unclassified element
       if A[equal] < pivot:
@@ -23,7 +23,7 @@ def dutch_flag_parition(pivot_index, A)
       else: #A[equal] > pivot
         larger -= 1
         A[equal], A[larger] = A[larger], A[equal]
-        
+
         #time complexity is O(n), space complexity O(1)
 
 #Another approach
@@ -41,4 +41,3 @@ def dutch_flag_partition2(pivot_index, A):
         elif A[i] > pivot:
             A[larger], A[i] = A[i], A[larger]
             larger -= 1
-            
