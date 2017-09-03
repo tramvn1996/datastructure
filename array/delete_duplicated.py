7f51f7e0a130499371d82c626d3cfc7117e3d313
Tram Nguyen <tramvn1996@gmail.com>
2017-09-03 00:57:44 -0400

    add a function to delete duplicated

---
+++ array/delete_duplicated.py
@@ -0,0 +1,14 @@
+#delete duplicated elements in an array
+def delete_duplicated(A):
+    if not A:
+        return 0
+
+    write_index = 1 #start writing at index 1 to compare with index 0
+    for i in range(1, len(A)):
+        if A[write_index - 1] != A[i]:
+            A[write_index] = A[i]
+            write_index += 1
+    return A
+
+B=[0,0,1,2,2,3,1]
+print(delete_duplicated(B))
