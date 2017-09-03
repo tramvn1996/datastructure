d2ece5ce55779fed5834ef278802c5d68901e7f9
Tram Nguyen <tramvn1996@gmail.com>
2017-09-02 23:20:42 -0400

    fix "remove leading zeroes"

--- array/multiply.py
+++ array/multiply.py
@@ -8,14 +8,17 @@ def multiply(num1, num2):
 
 #multiply
     for i in reversed(range(len(num1))):
-        for j reversed(range(len(num2))):
+        for j in reversed(range(len(num2))):
             result[i + j + 1] += num1[i] * num2[j]
             result[i +j] += result[i + j + 1]//10
             result[i+j+1] %= 10
 
 #remove leading zeroes
-    if result[0] == 0:
-        result = sign*result[1] + result[1:]
-    else:
-        result = sign*result[0] + result
-        
+    while result[0] == 0:
+        result = result[1:]
+
+    return [sign*result[0]]+result[1:]
+
+A=[1,2]
+B=[1,2]
+print(multiply(A,B))
