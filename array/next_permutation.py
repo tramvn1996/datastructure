b05dae468e2d908e6b93357ff5665fcb06e1d104
Tram Nguyen <tramvn1996@gmail.com>
2017-09-04 00:39:09 -0400

    add next_permutation

---
+++ array/next_permutation.py
@@ -0,0 +1,25 @@
+#a program that return the next permutation
+# essentially, find the smallest permutation that's larger than the orgiginal one
+# we do this by identifying where in the array that elements start decreasing
+
+def next_permutation(perm):
+    #find the first entry from the right
+    #that is smaller than the entry right after it
+
+    inversion_point = len(A)-2
+    while (inversion_point>=0 and perm[inversion_point]>=perm[inversion_point+1]):
+        inversion_point -= 1
+    if inversion_point == -1:
+        return [] #perm is the last permutation
+    for i in reversed(range(inversion_point+1, len(perm))):
+        if perm[i] > perm[inversion_point]:
+            perm[inversion_point], perm[i]=perm[i], perm[inversion_point]
+            break
+            #exchange the smallest value in the furthest right as possible
+            #to the inversion_point
+
+#entries in perm must appear in decreasing order after inversion_point
+#to yield smallest permutation, so we reversed the order
+    perm[inversion_point+1:]=reversed(perm[inversion_point+1:])
+    return perm
+    
