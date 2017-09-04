d1ea8735f7481580ded23e3c0cf356c62dee8ac8
Tram Nguyen <tramvn1996@gmail.com>
2017-09-03 22:21:42 -0400

    update generate prime

--- array/generate_primes.py
+++ array/generate_primes.py
@@ -3,11 +3,10 @@ def generate_prime(n):
     primes = []
     is_prime = [False, False] + [True]*(n-1)
     for p in range(2,n):
-        for i in range(p,n+1,p):
-            is_prime[i]=False
         if is_prime[p]:
             primes.append(p)
-
+            for i in range(p,n+1,p):
+                is_prime[i]=False
     return primes
 
 print(generate_prime(9))
