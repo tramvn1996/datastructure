55f7b0cf5ad9c67b3ab7a9f4b5edd541b37e1f04
Tram Nguyen <tramvn1996@gmail.com>
2017-09-04 16:23:18 -0400

    add a spreadsheet encode function

---
+++ strings/spreadsheet_encoding.py
@@ -0,0 +1,11 @@
+#convert a spreadsheet column into an int
+# essentially convert string into int base 26
+def spreadsheet_encoding(column_name):
+    index = 0
+    result = 0
+    for i in (reversed(range(len(column_name)))):
+        result += (ord(column_name[i])-ord('A')+1) * (26**index)
+        index += 1
+        print(result)
+    return result
+print(spreadsheet_encoding('AAA'))
