8a16ff0b63948f7b7be976bf0aac56c4e618dc9c
Tram Nguyen <tramvn1996@gmail.com>
2017-09-12 14:38:05 -0400

    add an example and time complexity

--- searching/compute_real_sq_root.py
+++ searching/compute_real_sq_root.py
@@ -1,4 +1,5 @@
 #write a function that computes the real square root
+import math
 def real_sq_root(x):
     #decide search range
     left, right = (x,1.0) if x < 1 else (1.0, x)
@@ -7,10 +8,12 @@ def real_sq_root(x):
     while not math.isclose(left, right):
         mid = (left+right) * 0.5
         mid_square = mid * mid
-        if mid_squre > x:
+        if mid_square > x:
             right = mid
         else:
             left = mid
 
     return left
-    
+
+print(real_sq_root(9))
+#time complexity is O(logx/s) s is the tolerance
