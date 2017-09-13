ecd8b1733a079e069348b4ab2d693e8485c65ceb
Tram Nguyen <tramvn1996@gmail.com>
2017-09-13 11:07:05 -0400

    add some explanation to code

--- binary_tree/an_inorder_traverseal_wo_recursion.py
+++ binary_tree/an_inorder_traverseal_wo_recursion.py
@@ -5,15 +5,16 @@ def bst_in_sorted_order(tree):
     s, result = [],[]
     while s or tree:
         if tree:
+            #add all the left subtree to s
             s.append(tree)
             #going left
             tree=tree.left
         else:
             #going up
             tree = s.pop()
-            result.append(tree.data)
+            result.append(tree.data) # append the s.pop() data into result
             #going right
-            tree=tree.right
+            tree=tree.right #add tree.right to tree->add to s -> append s.pop() to result
     return result
 
     #time O(n), space O(h)
