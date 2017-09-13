52e27bf0242d58e09daecd6ffa804d458afad4b9
Tram Nguyen <tramvn1996@gmail.com>
2017-09-13 12:27:08 -0400

    using hash table to test if a string can be palindrome

---
+++ hash_table/test_for_palin_string.py
@@ -0,0 +1,10 @@
+# test if a string can be permutated so that it'll become palindrome
+# condition:
+#if len(s) is odd -> 1 element need appear an odd number of times
+# if len(s) is even -> all elements need to appear an even number of times
+import collections
+
+def can_string_be_a_palindrome(s):
+    return sum(v%2 for v in collections.Counter(s).value()) <= 1
+#time O(n) n-length of the string
+#space O(c) c-> number of distinct character in the string
