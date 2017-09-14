6a6ae2c00058c6f1c41df7bb281be8adc361d107
Tram Nguyen <tramvn1996@gmail.com>
2017-09-14 13:00:47 -0400

    add a function to find the nearest repetition of a string in an array

---
+++ hash_table/nearest_repetition.py
@@ -0,0 +1,9 @@
+#find the nearest repetition of a word
+def find_nearest_repetition(paragraph):
+    word_to_latest_index, nearest_repeated_distance = {}, float('inf')
+    for i, word in enumerate(paragraph):
+        if word in word_to_latest_index:
+            latest_equal_word = word_to_latest_index[word]
+            nearest_repeated_distance = min(nearest_repeated_distance, i-latest_equal_word)
+        word_to_latest_index[word] = i
+    return nearest_repeated_distance
